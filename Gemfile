b784cbd00ed200190d36ec1d07aa88bac9fceaf4
archmagos <archmagosgit@outlook.com>
2020-05-02 09:12:46 +0100

    Code quality refactors

--- Gemfile
+++ Gemfile
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.3'
 
-gem 'rails', '~> 6.0.2', '>= 6.0.2.2'
 gem 'jbuilder', '~> 2.7'
 gem 'pry'
 gem 'puma', '~> 4.1'
+gem 'rails', '~> 6.0.2', '>= 6.0.2.2'
 gem 'rspec-rails', '~> 4.0.0'
 
 group :development do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
@@ -5,12 +7,12 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.log_level = :debug
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
